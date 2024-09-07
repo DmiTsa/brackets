ac1e8e0473e17afa2aad9382de6312b73011de26
DmiTsa <dmitrytaskaev.dt@gmail.com>
2024-09-07 07:45:58 +0300

    feat: 60% done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openSym = []
+  const closeSym = []
+  const control = []
+  let rez = true
+
+  const isEquals = (arrs, arr) => {
+    return arrs.map(el => el.toString() == arr.toString()).some(el => el === true)
+  }
+
+  bracketsConfig.forEach(el => {
+    openSym.push(el[0])
+    closeSym.push(el[1])
+  });
+
+  str.split('').forEach(el => {
+    if (openSym.includes(el)) { control.push(el) } else {
+      if (closeSym.includes(el)) {
+        if (control.length === 0) { rez = false } else {
+          const tmpArrPair = [control.pop(), el]
+
+          if (!isEquals(bracketsConfig, tmpArrPair)) { rez = false }
+        }
+
+      }
+    }
+
+  });
+
+  if (control.length !== 0) { rez = false }
+
+  return rez
 }
